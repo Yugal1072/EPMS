83411c488bf4f77b0f7305cd095332968e2a47dc
Yugal <yugalgovind1072@gmail.com>
2023-09-25 19:33:39 +0530

    FOrget password updated

--- app1/urls.py
+++ app1/urls.py
@@ -19,12 +19,45 @@ urlpatterns = [
     
     
     #reset password
-    path('reset_password/',auth_views.PasswordResetView.as_view(template_name = "registration/password_reset.html"),name='reset_password'),
+    # path('reset_password/',auth_views.PasswordResetView.as_view(template_name = "registration/password_reset.html"),name='reset_password'),
 
-    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name = "registration/password_reset_sent.html"),name='password_reset_done'), 
+    # path('reset_pa ssword_sent/',auth_views.PasswordResetDoneView.as_view(template_name = "registration/password_reset_sent.html"),name='password_reset_done'), 
 
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = "registration/password_reset_form.html"),name='password_reset_confirm'),
+    # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = "registration/password_reset_form.html"),name='password_reset_confirm'),
     
-    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name = "registration/password_reset_done.html"),name='password_reset_complete'),
+    # path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name = "registration/password_reset_done.html"),name='password_reset_complete'),
+
+    # path('reset_password/',
+    # auth_views.PasswordResetView.as_view(template_name = "accounts/password_reset.html"),
+    # name='reset_password'),
+
+    # path('reset_password_sent/',
+    # auth_views.PasswordResetDoneView.as_view(template_name = "accounts/password_reset_sent.html"),
+    # name='password_reset_done'), 
+ 
+    # path('reset/<uidb64>/<token>/',
+    # auth_views.PasswordResetConfirmView.as_view(template_name = "accounts/password_reset_form.html"),
+    # name='password_reset_confirm'),
+    
+    # path('reset_password_complete/',
+    # auth_views.PasswordResetCompleteView.as_view(template_name = "accounts/password_reset_done.html"),
+    # name='password_reset_complete'),
+
+
+    path('reset_password/',
+    auth_views.PasswordResetView.as_view(template_name = "accounts/password_reset.html"),
+    name='reset_password'),
+
+    path('reset_password_sent/',
+    auth_views.PasswordResetDoneView.as_view(template_name = "accounts/password_reset_sent.html"),
+    name='password_reset_done'), 
+ 
+    path('reset/<uidb64>/<token>/',
+    auth_views.PasswordResetConfirmView.as_view(template_name = "accounts/password_reset_form.html"),
+    name='password_reset_confirm'),
+    
+    path('reset_password_complete/',
+    auth_views.PasswordResetCompleteView.as_view(template_name = "accounts/password_reset_done.html"),
+    name='password_reset_complete'),
 
 ]
